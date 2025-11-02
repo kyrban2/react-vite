973fb61894dafea6bac98a278fa30a41b7bd8973
kyrban <kyrban1x@gmail.com>
2025-11-02 14:23:47 +0300

    final

--- vite-project/src/components/ImageComponent/ImageComponent.tsx
+++ vite-project/src/components/ImageComponent/ImageComponent.tsx
@@ -1,11 +1,11 @@
 const ImageComponent = () => {
 	return (
 		<>
-			<img src='/assets/logotip.png' alt='Logo' />
-			<img src='/assets/iras.png' alt='Iras' />
+			<img src='./assets/logotip.png' alt='Logo' />
+			<img src='./assets/iras.png' alt='Iras' />
 			<div
 				style={{
-					backgroundImage: `url(/assets/glav.jpg)`,
+					backgroundImage: `url('./assets/glav.jpg')`,
 					width: '300px',
 					height: '200px',
 					backgroundSize: 'cover',
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,17 +1,3 @@
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-// import { defineConfig } from 'vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-// 	plugins: [react()],
-// 	resolve: {
-// 		alias: {
-// 			'@': path.resolve(__dirname, './src'),
-// 			base: 'react-vite',
-// 		},
-// 	},
-// })
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
@@ -21,7 +7,17 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
-			base: 'react-vite',
+		},
+	},
+	base: '/final/',
+	build: {
+		rollupOptions: {
+			output: {
+				// Сохранять оригинальные имена файлов
+				chunkFileNames: '[name].js',
+				entryFileNames: '[name].js',
+				assetFileNames: '[name].[ext]',
+			},
 		},
 	},
 })
